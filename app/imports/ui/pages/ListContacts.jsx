5e4ec38af47d9f2b3c169c866fda670eaa8e8741
Thomas Yang <101thomasyang@gmail.com>
2020-11-08 23:21:03 -1000

    notes-1 finished in 36 minutes.

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -4,6 +4,7 @@ import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Contacts } from '../../api/contact/Contacts';
+import { Notes } from '../../api/note/Notes';
 import Contact from '../components/Contact';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -20,7 +21,9 @@ class ListContacts extends React.Component {
         <Container>
           <Header as="h2" textAlign="center" inverted>List Contacts</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+            {this.props.contacts.map((contact, index) => <Contact key={index}
+                                                                  contact={contact}
+                                                                  notes={this.props.notes.filter(note => (note.contactId === contact._id))}/>)}
           </Card.Group>
         </Container>
     );
@@ -30,15 +33,18 @@ class ListContacts extends React.Component {
 /** Require an array of Stuff documents in the props. */
 ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
+  notes: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Contacts.userPublicationName);
+  const subscriptionContacts = Meteor.subscribe(Contacts.userPublicationName);
+  const subscriptionNotes = Meteor.subscribe(Notes.userPublicationName);
   return {
     contacts: Contacts.collection.find({}).fetch(),
-    ready: subscription.ready(),
+    notes: Notes.collection.find({}).fetch(),
+    ready: subscriptionContacts.ready() && subscriptionNotes.ready(),
   };
 })(ListContacts);
